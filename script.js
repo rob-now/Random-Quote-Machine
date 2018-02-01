7df62553561436e15032eecede2fe4d55ed39382
rob-now <rnowakowski.prv@gmail.com>
2018-02-01 11:08:41 +0100

    Added tweet functionality and tweet button

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ function quotes() {
     $.ajax( {
         url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&',
         success: function(data) {
-            var post = data.shift(); // The data is an array of posts. Grab the first one.
+            post = data.shift(); // The data is an array of posts. Grab the first one.
             $('#quote-content').html(post.content);
             $('#quote-title').html("— " + post.title);
         },
         cache: false
     });
 }
+
+function tweet() {
+    var quoteToTweet = post.content.replace(/<\/?[^>]+(>|$)/g, "") + post.title;
+    if (quoteToTweet.length > 140) {
+        alert('Tweet should be less than 140 characters.');
+    } else {
+        var twtLink = 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent(quoteToTweet);
+        window.open(twtLink, '_blank');
+    }
+}
+
 $(document).ready(function() {
     quotes();
     $('#get-new-quote-btn').click(quotes);
+    $('#tweet-btn').click(tweet);
 });
